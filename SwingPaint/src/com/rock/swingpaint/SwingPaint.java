823e0d457d69c9f73cdfcbd2435ddd02851495b2
Fangxian Gu <gfx505879985@gmail.com>
2017-01-11 21:38:48 -0500

    Update SwingPaint.java
    added menu bar

--- SwingPaint/src/com/rock/swingpaint/SwingPaint.java
+++ SwingPaint/src/com/rock/swingpaint/SwingPaint.java
@@ -12,6 +12,78 @@ public class SwingPaint
 	
 	public void show()
 	{
+		//make menu bar
+		JMenuBar menuBar = new JMenuBar();
+		JMenu menu = new JMenu("File");
+
+		JMenuItem menuItem = new JMenuItem("New File");
+		menu.add(menuItem);
+		menuItem = new JMenuItem("Open");
+		menuItem.setMnemonic(KeyEvent.VK_S);
+		menu.add(menuItem);
+		menuItem = new JMenuItem("Save");
+		
+		menu.add(menuItem);
+		menu.addSeparator();
+		menuItem = new JMenuItem("Exit");
+		menuItem.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				System.exit(0);
+			}
+		});
+		menu.add(menuItem); 
+		menuBar.add(menu);
+		
+		menu = new JMenu("Edit");
+		menuItem = new JMenuItem("Undo");
+		menu.add(menuItem);
+		menuItem = new JMenuItem("Clear All");
+		menuItem.setAccelerator(KeyStroke.getKeyStroke(
+		        KeyEvent.VK_DELETE, ActionEvent.ALT_MASK));
+		menuItem.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				drawShape.clear();
+			}
+		});
+		menu.add(menuItem);
+		
+		//make subMenu
+		JMenu subMenu = new JMenu("Color");
+		menuItem = new JMenuItem("Black");
+		menuItem.addActionListener(colorHandler(Color.black));
+		subMenu.add(menuItem);
+		menuItem = new JMenuItem("LightGrey");
+		menuItem.addActionListener(colorHandler(Color.lightGray));
+		subMenu.add(menuItem);
+		menuItem = new JMenuItem("White");
+		menuItem.addActionListener(colorHandler(Color.white));
+		subMenu.add(menuItem);
+		menuItem = new JMenuItem("Blue");
+		menuItem.addActionListener(colorHandler(Color.blue));
+		subMenu.add(menuItem);
+		menuItem = new JMenuItem("Yellow");
+		menuItem.addActionListener(colorHandler(Color.yellow));
+		subMenu.add(menuItem);
+		menuItem = new JMenuItem("Red");
+		menuItem.addActionListener(colorHandler(Color.red));	
+		subMenu.add(menuItem);
+		subMenu.addSeparator();
+		menuItem = new JMenuItem("Choose...");
+		subMenu.add(menuItem);
+		//add sub menu to main menu
+		menu.add(subMenu);
+		
+		subMenu = new JMenu("Insert");
+		menuItem = new JMenuItem("Text");
+		subMenu.add(menuItem);
+		menuItem = new JMenuItem("Picture");
+		subMenu.add(menuItem);
+		menu.add(subMenu);
+		menuBar.add(menu);
 		//make frame
 		JFrame frame = new JFrame("Swing Paint");
 		//make container for layout
@@ -41,6 +113,16 @@ public class SwingPaint
 		frame.setVisible(true);
 	}
 	
+	public ActionListener colorHandler(final Color c)
+	{
+		return new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				drawShape.setColor(c);
+			}
+		};
+	}
 	
 	public static void main(String[] args)
 	{
